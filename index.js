24a171fb61b13f563ef606d77166f357a518c0f9
lieonlion <lieonlion4@gmail.com>
2024-10-02 20:10:48 +0100

    boop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const headers = {
     'Accept':'application/json',
-    'x-api-key':'$2a$10$rVnnGf6WztLzli2pU3xIt.LeNYGvuVo3qGgQvQNb.ua3GrUOFXr2m'
+    'x-api-key':'$2a$10$X5BedJMwNKGRXoetXlscbOhfxNMZmSXiLDwMFvlHTMM0xqCa5pUvG'
 };
 
 let getCurseforgeData = async function (id) {
